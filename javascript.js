bd5ca71e8ab5a19177cfa6b0b14d39e1670639c2
Michael D. Kennedy <mdkennedy1@gmail.com>
2015-12-23 16:25:56 -0500

    css and js updates

--- javascript.js
+++ javascript.js
@@ -1,7 +1,48 @@
 $(document).ready(function(){
-	$("body").append("<div id='container'></div>");
-	
-	for var (i = 0; i < 4; i++) {
-	$('#container').append("<div id='square'></div>");
+	var squares = 16;
+	var count = 0;  
+
+	$('body').append('<button type="button" id="reset">Reset</button>');
+	$('body').append('<div id="container"></div>');
+
+function load() {
+		while (count < squares) {
+			count += 1;
+			$('#container').append('<div class = "screen" id="row'+ count + '"></div>');
+			var count1 = 0;
+			while (count1 < squares) {
+				$('#row' + count).append ('<div class = "screen">');
+				count1 += 1;
+			}
+		}
 	}
+
+	load();
+
+	$(document).on('click',':button', function screenReload() {
+		squares = prompt("Please input the size of the new screen. 1 - 128","16");
+		while (squares < 1 || squares > 128) {
+			squares = prompt("Please input the size of the new screen.1 - 128","16");
+		}
+		$('#container').empty();
+		$(".screen").css('background-color','#454343');
+		var dimension = 500 / squares;
+		count = 0;
+		load();
+		$(".screen").css('height',dimension);		
+		$(".screen").css('width',dimension);
+		pen();
+});
+
+
+
+
+	function pen() {
+		$('.screen').hover(function () {
+			$(this).css('background-color', "#000");
+		});
+	}
+
+	pen();
+
 });
